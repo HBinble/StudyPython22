b12179a82dd31d291158053a0278f8a172bd54bf
HanBin1205 <diary1205@naver.com>
2022-05-02 11:49:40 +0900

    220502 파이썬 기본 문법학습

---
+++ basic/datatype02.py
@@ -0,0 +1,12 @@
+# 문자열
+bruce_ekcel = 'Life is short.\n\tYou need python.'
+print(bruce_ekcel)
+
+# 여러줄 문자열
+multi_line = '''Life is short.
+You need Python.
+And I need c#, too.
+'''
+print(multi_line)
+
+print(type(bruce_ekcel))

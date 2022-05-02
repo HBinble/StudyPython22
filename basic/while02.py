64bfabee410bf5f70cae97bc3a2d1fb2980b8550
HanBin1205 <diary1205@naver.com>
2022-05-02 17:49:00 +0900

    220502 기본문법학습완료

--- basic/while02.py
+++ basic/while02.py
@@ -12,17 +12,17 @@ while True:
 입력번호 : ''')
     num = int(input()) #키보드로 입력바든 수를 num 할당
 
-if num == 1:
-    print('회원정보입력')   
-elif num == 2: # 1은 아니고 2이면 
-    print('회원검색')
-elif num == 3: # 1,2도 아니고 3이면 
-    print('회원수정')
-elif num == 4: # 1,2,3도 아니고 4이면 
-    print('회원삭제')
-elif num == 5:
-    print('프로그램종료')
-    break
-else:
-    print('잘못 입력했습니다')
-    continue
+    if num == 1:
+        print('회원정보입력')   
+    elif num == 2: # 1은 아니고 2이면 
+        print('회원검색')
+    elif num == 3: # 1,2도 아니고 3이면 
+        print('회원수정')
+    elif num == 4: # 1,2,3도 아니고 4이면 
+        print('회원삭제')
+    elif num == 5:
+        print('프로그램종료')
+        break
+    else:
+        print('잘못 입력했습니다')
+        continue

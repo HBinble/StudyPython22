152495a1c2c725d61370ccb054450489b89bcc05
HanBin1205 <diary1205@naver.com>
2022-05-03 17:01:07 +0900

    220503 파이썬 중간 문법학습

---
+++ intermediate/03.module01.py
@@ -0,0 +1,14 @@
+# 모듈 학습
+# import math as m # 방법 1(import~)
+
+# print(m.pi)
+# print(m.log10(30))
+# print(m.pow(2,10))
+# print(2 ** 10)
+
+from math import pow, log10, pi # 방법 2(from ~ import~)
+print(pi)
+print(log10(30))
+print(pow(2,10)) # = print(2 ** 10)
+
+

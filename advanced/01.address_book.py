f8f106eee52711daada8aba90d7251487c812d07
HanBin1205 <diary1205@naver.com>
2022-05-06 17:47:09 +0900

    220506 TGIF!!

--- advanced/01.address_book.py
+++ advanced/01.address_book.py
@@ -24,6 +24,7 @@ class Contact:
     
 def run():
     lst_contact = [] # 빈 리스트 생성
+    load_contact(lst_contact) # 파일DB 읽어오기
     # contact = Contact('홍길동', '010-0000-1111', 'hdg@naver.com', '서울')
     # print(contact)
     #set_contact()
@@ -33,22 +34,28 @@ def run():
         sel_menu = get_menu()
         if sel_menu == 1:
             clearConsole()
-            contect = set_contact()
-            lst_contact.append(contect)
-            input('입력이 완료되었습니다.') # 아무값도 안받고 있는 엔터 대기
+            contact = set_contact()
+            if contact is None : 
+                input('계속하려면 엔터를 누르세요')
+                clearConsole()
+                continue #contact가 비면 리스트 추가 불가
+
+            lst_contact.append(contact)
+            input('연락처 추가성공\n계속하려면 엔터를 누르세요') # 아무값도 안받고 있는 엔터 대기
             clearConsole()
         elif sel_menu == 2: #연락처 출력
             clearConsole()
             get_contact(lst_contact)
-            input('출력이 완료되었습니다.') # 엔터 대기
+            input('계속하려면 엔터를 누르세요') # 엔터 대기
             clearConsole()
         elif sel_menu == 3:
             clearConsole()
             name = input('삭제할 이름을 입력하세요 : ')
             del_contact(lst_contact, name)
-            input('삭제가 완료되었습니다.')
+            input('연락처 삭제성공\n계속하려면 엔터를 누르세요')
             clearConsole()        
-        elif sel_menu == 4:
+        elif sel_menu == 4: # 종료
+            save_contact(lst_contact) # 파일DB저장
             break
         else:
             clearConsole()
@@ -57,13 +64,15 @@ def run():
 
 # 주소록 입력함수
 def set_contact():
-    name, phone_num, e_mail, addr = \
+    contact = None
+    try: #아무입력없이 엔터 | 갯수 틀리면 생기는 예외처리
+        name, phone_num, e_mail, addr = \
         input('정보입력(이름,핸드폰,이메일,주소(구분자 /)) >').split('/')
-    contact = Contact(name, phone_num, e_mail, addr)
-    return contact
-    # contact = Contact(phone_num=phone_num, e_mail=e_mail,
-    #                 name=name, addr=addr)
-    # print(contact)
+        contact = Contact(name, phone_num, e_mail, addr)
+    except Exception as e:
+        print('입력갯수를 확인(이름/핸드폰/이메일/주소)')
+    finally:
+        return contact # 잘못되면 None 리턴, Contact 객체 리턴
 
 # 주소록 전체 출력
 def get_contact(lst_contact):
@@ -76,6 +85,32 @@ def del_contact(lst_contact, name):
         if contact.name == name:
             del lst_contact[i]      
 
+# 주소록 파일DB 저장
+def save_contact(lst_contact):
+    f = open('./advanced/db_contact.txt', mode='w', encoding='utf-8')
+    for contact in lst_contact:
+        f.write(contact.name + '/')
+        f.write(contact.phone_num + '/')
+        f.write(contact.e_mail + '/')
+        f.write(contact.addr + '\n')
+
+    f.close()
+
+# 주소록 파일DB 로드
+def load_contact(lst_contact:list):
+    f = open('./advanced/db_contact.txt', mode='r', encoding='utf-8')
+    while True:
+        line = f.readline()
+        if not line: break
+
+        lines = line.rstrip('\n').split('/') # \n제거하고, /로 나누고 리스트로 
+        if len(lines) !=4: continue #220506 11:11 Enter로 생기는 예외처리
+        contact  = Contact(lines[0], lines[1], lines[2], lines[3])
+        lst_contact.append(contact)
+
+    f.close()
+    
+# 매뉴 출력 및 선택
 def get_menu():
     str_menu = ('--주소록 프로그램 v.1.1--\n'
                 '1. 연락처 추가\n'
@@ -83,8 +118,14 @@ def get_menu():
                 '3. 연락처 삭제\n'
                 '4. 종료\n')
     print(str_menu)
-    menu = input('메뉴입력 : ')
-    return int(menu)
+    #0~9 숫자 외에는 ValueError 발생
+    manu = 0 #초기화
+    try:
+        menu = int(input('메뉴입력 : '))
+    except Exception as e:
+        print(e)
+    finally:
+        return menu
 
 def clearConsole():
     command = 'clear'  # mac, unix, linux 화면클리어 명령어
@@ -92,6 +133,9 @@ def clearConsole():
         command = 'cls' # windows, dos 화면클리어 명령어
     os.system(command)
 
-
 if __name__ == '__main__':
+    try:
         run()
+    except KeyboardInterrupt as e:
+        clearConsole()
+        print('Ctrl+c 종료')
